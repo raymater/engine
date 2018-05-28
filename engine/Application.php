3b4445171c342dc12e5af837a49ff1abcac6e9c0
raymater <raymater@outlook.fr>
2018-05-28 23:48:58 +0200

    PHPDoc for Application class
    Public functions have been documented ^^.

--- engine/Application.php
+++ engine/Application.php
@@ -13,7 +13,16 @@ class Application
 	protected $globalVars = array();
 	protected $timezone = "GMT";
 	
-	public function __construct($_config) {
+	/**
+		* Create the main application
+		*
+		* Make the configuration for your app by config array passed on param. Also set the base path for URL, the PHP version and the name of the current server software.
+		*
+		* @param array $_config
+		*	The config array
+		* @return void
+	**/
+	public function __construct($_config = array()) {
 		$this->routes = array();
 		
 		if(array_key_exists("debug", $_config)) {
@@ -91,30 +100,111 @@ class Application
 		}
 	}
 	
+	/**
+		* Create a GET route
+		*
+		* Make a route with GET method while specifying URL and the action.
+		*
+		* @param string $_url
+		*	Your URL (start with "/").
+		* @param callable $_action
+		*	What do you want to do on this URL ? (function(Request $req, Response $resp, $args, $app) { // Do Something }).
+		* @return Route
+		*	The Route object created.
+	**/
 	public function get($_url, $_action) {
 		return $this->route($_url, $_action, "GET");
 	}
 	
+	/**
+		* Create a POST route
+		*
+		* Make a route with POST method while specifying URL and the action.
+		*
+		* @param string $_url
+		*	Your URL (start with "/").
+		* @param callable $_action
+		*	What do you want to do on this URL ? (function(Request $req, Response $resp, $args, $app) { // Do Something }).
+		* @return Route
+		*	The Route object created.
+	**/
 	public function post($_url, $_action) {
 		return $this->route($_url, $_action, "POST");
 	}
 	
+	/**
+		* Create a PUT route
+		*
+		* Make a route with PUT method while specifying URL and the action.
+		*
+		* @param string $_url
+		*	Your URL (start with "/").
+		* @param callable $_action
+		*	What do you want to do on this URL ? (function(Request $req, Response $resp, $args, $app) { // Do Something }).
+		* @return Route
+		*	The Route object created.
+	**/
 	public function put($_url, $_action) {
 		return $this->route($_url, $_action, "PUT");
 	}
 	
+	/**
+		* Create a DELETE route
+		*
+		* Make a route with DELETE method while specifying URL and the action.
+		*
+		* @param string $_url
+		*	Your URL (start with "/").
+		* @param callable $_action
+		*	What do you want to do on this URL ? (function(Request $req, Response $resp, $args, $app) { // Do Something }).
+		* @return Route
+		*	The Route object created.
+	**/
 	public function delete($_url, $_action) {
 		return $this->route($_url, $_action, "DELETE");
 	}
 	
+	/**
+		* Create a PATCH route
+		*
+		* Make a route with PATCH method while specifying URL and the action.
+		*
+		* @param string $_url
+		*	Your URL (start with "/").
+		* @param callable $_action
+		*	What do you want to do on this URL ? (function(Request $req, Response $resp, $args, $app) { // Do Something }).
+		* @return Route
+		*	The Route object created.
+	**/
 	public function patch($_url, $_action) {
 		return $this->route($_url, $_action, "PATCH");
 	}
 	
+	/**
+		* Create a OPTIONS route
+		*
+		* Make a route with OPTIONS method while specifying URL and the action.
+		*
+		* @param string $_url
+		*	Your URL (start with "/").
+		* @param callable $_action
+		*	What do you want to do on this URL ? (function(Request $req, Response $resp, $args, $app) { // Do Something }).
+		* @return Route
+		*	The Route object created.
+	**/
 	public function options($_url, $_action) {
 		return $this->route($_url, $_action, "OPTIONS");
 	}
 	
+	/**
+		* Set a 404 default route for the app
+		*
+		* Set action to do when a ressource is not found.
+		*
+		* @param callable $_action
+		*	What do you want to do ? (function(Request $req, Response $resp, $args, $app) { // Do Something }).
+		* @return void
+	**/
 	public function error404($_action) {
 		if(is_callable($_action)) {
 			$this->route404 = $_action;
@@ -125,22 +215,66 @@ class Application
 		}
 	}
 	
+	/**
+		* Get the base URL
+		*
+		* Return the base URL/path for your app.
+		*
+		* @return string
+		*	The base path
+	**/
 	public function baseURL() {
 		return $this->_baseURL;
 	}
 	
+	/**
+		* Set a global var
+		*
+		* Put or replace a value accessible on the entire application.
+		*
+		* @param string $_name
+		*	Name of your global var
+		* @param mixed $_value
+			A value to save
+		* @return mixed
+		*	The value saved ($_value)
+	**/
 	public function setVar($_name, $_value) {
 		$this->globalVars[$_name] = $_value;
+		return $this->globalVars[$_name];
 	}
 	
+	/**
+		* Delete a global var by name
+		*
+		* Delete a value accessible on the entire application.
+		*
+		* @param string $_varName
+		*	Name of your global var
+		* @return bool
+		*	Return true if the method has found a the var, false if not.
+	**/
 	public function deleteVar($_varName) {
+		$resp = false;
 		foreach ($this->globalVars as $key => $value) {
 			if($key === $_varName) {
 				unset($array[$key]);
+				$resp = true;
 			}
 		}
+		return $resp;
 	}
 	
+	/**
+		* Get global var
+		*
+		* Get a global var by name or get all global vars (set $_varName to null).
+		*
+		* @param string $_varName
+		*	Name of your global var. If is null (by default) = return all global vars.
+		* @return mixed
+		*	Return the value of the global var or an associative array representing all global vars.
+	**/
 	public function getVar($_varName = null) {
 		if($_varName != null) {
 			$valueVar = null;
@@ -156,28 +290,84 @@ class Application
 		}
 	}
 	
+	/**
+		* Get the debug setting
+		*
+		* Return a boolean value representing if the app is on debug mode or not.
+		*
+		* @return bool
+		*	Return true if is app is on debug mode, false if not.
+	**/
 	public function isDebug() {
 		return $this->debug;
 	}
 	
+	/**
+		* Get the PHP version
+		*
+		* Return the current PHP version running on the server.
+		*
+		* @return string
+		*	Return the PHP Version.
+	**/
 	public function getPhpVersion() {
 		return $this->PHPversion;
 	}
 	
+	/**
+		* Get the name of the server software
+		*
+		* Return the name of the server software
+		*
+		* @return string
+		*	Return the name of the server software.
+	**/
 	public function getServerSoftware() {
 		return $this->serverSoftware;
 	}
 	
+	/**
+		* Get the default timezone
+		*
+		* Return the default timezone identifier defined on the app (GMT by default).
+		*
+		* @return string
+		*	Return the name of the timezone identifier.
+	**/
 	public function getTimezone() {
 		return $this->timezone;
 	}
 	
+	/**
+		* Set the default timezone
+		*
+		* Change the timezone identifier for the app.
+		*
+		* @param string $_timezone
+		*	Name of your timezone identifier (by default : GMT).
+		* @return string
+		*	Return the name of the current timezone identifier.
+	**/
 	public function setTimezone($_timezone = "GMT") {
 		$this->timezone = $_timezone;
-		date_default_timezone_set($this->timezone);
+		$res = date_default_timezone_set($this->timezone);
+		if($res == false) {
+			$this->timezone = "GMT";
+			date_default_timezone_set($this->timezone);
+		}
 		return $this->timezone;
 	}
 	
+	/**
+		* Get route
+		*
+		* Return a specific route by its name or all routes available (null).
+		*
+		* @param string $_name
+		*	Name of your route (by default : null).
+		* @return mixed
+		*	Return the Route object of your route, or null if is not found or an array of Route object if $_name param is null
+	**/
 	public function getRoute($_name = null) {
 		if($_name != null) {
 			$thisRoute = null;
@@ -194,10 +384,26 @@ class Application
 		}
 	}
 	
+	/**
+		* Get the language
+		*
+		* Return the language of the app (used on Content-Language HTTP header) defined by RFC 5646. "en" by default or defined language on config app.
+		*
+		* @return string
+		*	Return the language (RFC 5646).
+	**/
 	public function getLang() {
 		return $this->lang;
 	}
 	
+	/**
+		* Run the application
+		*
+		* Running the application to match the current URL route with all defined route and execute middlewares and the Route function defined. If Route is not found, run the 404 route action.
+		*
+		* @return Application
+		*	Return the Application object.
+	**/
 	public function run() {
 		$requestMethod = $_SERVER['REQUEST_METHOD'];
 		
